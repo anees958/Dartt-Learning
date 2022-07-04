d793168261f2dd72c3cc05935d6d1dc32774e68b
anees958 <infohubservices186@gmail.com>
2022-07-04 15:37:19 +0500

    Basic

---
+++ bin/basic.dart
@@ -0,0 +1,60 @@
+void main() {
+  print("Hellow Dart");
+  int qun = 10;
+  double qunn = 10.000;
+  print(qun);
+  print(qunn);
+
+  String name = "Anees";
+  print("name");
+
+  var firstname = 8882;
+
+  print(firstname);
+
+  bool abc = false;
+  print(abc);
+
+  // Arthimatic Operators
+
+  var a = 100;
+  var b = 200;
+
+  print(a + b);
+  print(a * b);
+  print(a / b);
+
+  //Relational Operators
+
+  var c = 22;
+  var d = 28;
+  var e = 22;
+
+  print(c == d);
+  print(c == e);
+  print(c != e);
+  print(d > c);
+  print(c < e);
+
+  // Logical  Operators
+
+  // And Operators
+
+// true && true=True
+  //true && fakse=false
+  // false && true=false
+  // false&& false=False
+
+  // Or  Opeartoer
+
+// true || true=True
+  //true ||  fakse=true
+  // false || true=true
+  // false ||false=False
+
+  print(c == d && c == e);
+
+  print(c == d || c == e);
+
+  print(c.bitLength);
+}
